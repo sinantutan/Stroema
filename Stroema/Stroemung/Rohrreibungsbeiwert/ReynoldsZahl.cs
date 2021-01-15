a21dedb6ed4265b77e2352434862829573f9e77e
Moritz Kampfinger <s0569012@htw-berlin.de>
2021-01-15 19:56:55 +0100

    Bibliothek fuer die Berechnungen

---
+++ Stroema/Stroemung/Rohrreibungsbeiwert/ReynoldsZahl.cs
@@ -0,0 +1,18 @@
+﻿namespace Stroemung.Rohrreibungsbeiwert
+{
+{
+	public class ReynoldsZahl
+	{
+		public float value { get; }
+
+		public ReynoldsZahl() { }
+
+		public ReynoldsZahl(float v, float d, float nu) { value = calculate(v, d, nu); }
+
+		public ReynoldsZahl(float rho, float v, float d, float eta) { value = calculate(v, d, dyn_to_kin_viscosity(eta, rho)); }
+
+		public float calculate(float v, float d, float nu) { return v * d / nu; }
+
+		public float dyn_to_kin_viscosity(float eta, float rho) { return eta / rho; }
+	}
+}
